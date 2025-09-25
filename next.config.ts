d0382dc225b48fed4956ad51594c2fa5f5147885
Coomy Chang <coomysky@gmail.com>
2025-09-25 21:01:59 +0800

    chore: deploy docker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    PORT: "3000",
+  },
+  // 明確設置端口
+  serverRuntimeConfig: {
+    port: 3000,
+  },
+  // 確保在生產環境中也可用
+  publicRuntimeConfig: {
+    port: 3000,
+  },
 };
 
 export default nextConfig;
