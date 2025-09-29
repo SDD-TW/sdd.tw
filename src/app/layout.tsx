6de05bff148785aca55f65b158feaae6a2481636
Coomy Chang <coomysky@gmail.com>
2025-09-29 23:12:15 +0800

    feat: add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Analytics from "@/components/Analytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,17 +38,20 @@ export default function RootLayout({
   return (
     <html lang="zh-TW" suppressHydrationWarning={true}>
       <head>
+        {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TSHD5WVV');` }} />
+        
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSHD5WVV"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
+        <Analytics />
         {children}
       </body>
     </html>
